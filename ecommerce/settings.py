0d92eb5b4965313a3679b36b8cab0f253545d780
Daniel YL YIP <2602505y@student.gla.ac.uk>
2021-10-31 17:14:05 +0000

    home page banner

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -138,6 +138,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# need?
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATICFILES_DIRS = ("C:\\Users\\daniel\\Workspace\\ecommerce\\static", )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
