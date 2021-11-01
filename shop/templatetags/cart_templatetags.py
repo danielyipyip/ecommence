e88f6671a6ff477283d3ec82696f06b4c23f67c1
Daniel YL YIP <2602505y@student.gla.ac.uk>
2021-11-01 11:43:54 +0000

    added order count for shopper by template tag

--- shop/templatetags/cart_templatetags.py
+++ shop/templatetags/cart_templatetags.py
@@ -10,3 +10,12 @@ def cart_item_count(user):
         if querySet.exists():
             return querySet[0].orderitems.count()
     return 0
+
+@register.filter
+def order_count(user):
+    if user.groups.exists():
+        group=user.groups.all()[0].name #only 1 role for now
+        if group in 'shop_admin':
+            querySet = Order.objects.filter(complete=False)
+            return querySet.count()
+    return 0
