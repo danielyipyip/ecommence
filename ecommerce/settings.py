cf310cb3e5bff25ad789998350762c8990c3b2b1
Daniel YL YIP <2602505y@student.gla.ac.uk>
2021-11-27 18:55:51 +0000

    try solve heroku static issue by add in pj url.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -147,10 +147,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
-# need?
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATICFILES_DIRS = ("C:\\Users\\daniel\\Workspace\\ecommerce\\static", )
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 AUTHENTICATION_BACKENDS = [
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')), 
     path('shop/', include('shop.urls')), 
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
