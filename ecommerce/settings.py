a21c456f01b926ac4f53e01934c1c3f6755751f5
Daniel YL YIP <2602505y@student.gla.ac.uk>
2021-11-27 20:53:38 +0000

    fix a path in homepage

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -42,6 +42,7 @@ ALLOWED_HOSTS = ['danielyip-ecommerce.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
